4706d6499907287e4aa3310beb5629198b3a693b
Naing Lin Aung <nainglinaung91@gmail.com>
2014-07-08 15:27:07 +0630

    Change MongoClient from Mongo
    Use MongoClient instead of Mongo since it is deprecate

--- mongo/list.php
+++ mongo/list.php
@@ -26,8 +26,8 @@ define('MONGO_LIST_MAX_PAGE_SIZE',false); // set to a number to enforce a max pa
 function mongoList($server, $db, $collection, $select = null) {
 
   try {
-
-    $conn = new Mongo($server);
+    
+    $conn = new MongoClient($server);
     $_db = $conn->{$db};
     $collection = $_db->{$collection};
     
